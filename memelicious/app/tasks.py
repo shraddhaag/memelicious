3dcf090a1c49fa95b59799f63b725e395e0438dd
Shraddha Agrawal <shraddha.agrawal000@gmail.com>
2019-11-26 03:01:06 +0530

    Add async task to fetch YT video uploaded in past 1 minute
    This commit adds:
    
    1. The function `get_youtube_video` which returns all videos
    under *meme* topic uploaded less than one minute ago from current
    time.
    
    2. Add a sample config.yml.example file as an example to how a
    config.yml file is expected that houses API key.
    
    3. Modify .gitignore to ignore the config.yml file.
    
    Signed-off-by: Shraddha Agrawal <shraddha.agrawal000@gmail.com>

--- memelicious/app/tasks.py
+++ memelicious/app/tasks.py
@@ -1,5 +1,38 @@
+import os
+import datetime
+import googleapiclient.discovery
 from celery import shared_task
+from django.http import JsonResponse
+import yaml
+
+with open("config.yml", 'r') as ymlfile:	
+    config = yaml.full_load(ymlfile)
 
 @shared_task
 def get_videos(x,y):
     return x+y
+@shared_task
+def get_youtube_video():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = config['api-key']
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+
+    time_to_get_videos_from = (datetime.datetime.utcnow()-datetime.timedelta(minutes=1)).isoformat("T")+"Z"
+
+    request = youtube.search().list(
+        part="snippet",
+        order="date",
+        publishedAfter=time_to_get_videos_from,
+        q="memes",
+        type="video"
+    )
+    response = request.execute()
+
+    return response
